d51e1399e7de57b780375ca98ddff8b6c8b49003
Patrick Lurk <patricklurk@yahoo.com>
2024-03-18 22:20:50 -0500

    changed code

--- src/components/Payouts/Payouts.jsx
+++ src/components/Payouts/Payouts.jsx
@@ -1,5 +1,34 @@
+import { useEffect, useRef, useState } from 'react';
+
+import Button from '../UI/Button.jsx';
+
+console.log("in Payouts.jsx::outside of the Payouts component");
+
 export default function Payouts() {
+    console.log("in Payouts.jsx::beginning of component");
+
+    const [dataValue, setDataValue] = useState(false);
+
+    useEffect(() => {
+        console.log("in Payouts.jsx::useEffect()::beginning");
+
+        console.log("in Payouts.jsx::useEffect()::end");
+        }, [dataValue]
+    );
+
+    function handleClick() {
+        console.log("in Payouts.jsx::handleClick()");
+        console.log("in Payouts.jsx::handleClick() **" + (dataValue ? "true" : "false") + "**")
+        setDataValue(!dataValue);
+    }
+
+    console.log("in Payouts.jsx::end of component");
+
     return (
-        <h2>Payouts</h2>
+        <div>
+            <h2>Payouts</h2>
+            <p>Click the button to cause the useEffect() function to execute.</p>
+            <Button onClick={handleClick}>Click</Button>
+        </div>
     );
 }
