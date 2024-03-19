c126123dcff07faf8403c37e6704e2e3f5caeacb
Patrick Lurk <patricklurk@yahoo.com>
2024-03-18 23:51:39 -0500

    changed code

--- src/components/Payouts/Payouts.jsx
+++ src/components/Payouts/Payouts.jsx
@@ -9,26 +9,33 @@ export default function Payouts() {
 
     const [dataValue, setDataValue] = useState(false);
 
+    console.log("in Payouts.jsx::beginning of component dataValue = **" + (dataValue ? "true" : "false") + "**");
+    
     useEffect(() => {
         console.log("in Payouts.jsx::useEffect()::beginning");
-
+        console.log("in Payouts.jsx::useEffect() **" + (dataValue ? "true" : "false") + "**");
         console.log("in Payouts.jsx::useEffect()::end");
         }, [dataValue]
     );
 
     function handleClick() {
         console.log("in Payouts.jsx::handleClick()");
-        console.log("in Payouts.jsx::handleClick() **" + (dataValue ? "true" : "false") + "**")
+        console.log("in Payouts.jsx::handleClick()::before setDataValue **" + (dataValue ? "true" : "false") + "**")
         setDataValue(!dataValue);
+        console.log("in Payouts.jsx::handleClick()::after setDataValue **" + (dataValue ? "true" : "false") + "**")
     }
 
     console.log("in Payouts.jsx::end of component");
+    let buttonTextColor = dataValue ? 'red': 'yellow';
+    let buttonBackgroundColor = dataValue ? 'yellow': 'red';
 
     return (
         <div>
             <h2>Payouts</h2>
-            <p>Click the button to cause the useEffect() function to execute.</p>
-            <Button onClick={handleClick}>Click</Button>
+            <div style = {{border: '3px dashed blue'}}>
+                <p>Click the button to cause the useEffect() function to execute.</p>
+                <Button style = {{color: buttonTextColor, backgroundColor: buttonBackgroundColor, fontWeight: 'bold'}} onClick={handleClick}>Click</Button>
+            </div>
         </div>
     );
 }
