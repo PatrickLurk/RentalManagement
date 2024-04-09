582fe4f0e787d7813654860b6338ea39cf6ac62c
Patrick Lurk <patricklurk@yahoo.com>
2024-04-09 13:01:44 -0500

    more changes

--- src/components/Weather/Weather.jsx
+++ src/components/Weather/Weather.jsx
@@ -3,37 +3,78 @@ import axios from 'axios';
 
 import './../../index-weather.css';
 
-//const url = `https://api.openweathermap.org/data/2.5/weather?q=Destin&appid=0c0aa909068afe8c032d27e5c4cdf47c`;
+console.log("in Weather.jsx::outside of the Weather component");
 
 export default function Weather() { 
+    console.log("in Weather.jsx::beginning of component");
+
+    const [data, setData] = useState("");
+    const [location, setLocation] = useState("");
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=0c0aa909068afe8c032d27e5c4cdf47c`;
+
+    const searchLocation = (event) => {
+        console.log("in Weather.jsx::searchLocation() - beginning");
+
+        if (event.key === 'Enter') {
+            console.log("searchLocation()::event.target.value = **" + event.target.value + "**");
+
+            axios.get(url).then((response) => {
+                setData(response.data)
+                console.log(response.data)
+            })
+            setLocation('');
+        }
+
+        console.log("in Weather.jsx::searchLocation() - end");
+    }
+    const handleLocationChange = ({ target }) => {
+        console.log("in Weather.jsx::handleLocationChange() - beginning");
+        console.log("in Weather.jsx::handleLocationChange()::target.value = **" + target.value + "**");
+        setLocation(target.value)
+        console.log("in Weather.jsx::handleLocationChange() - end");
+      };
+
+    console.log("in Weather.jsx::end of component");
+
     return (
         <div className="weather">
+            <div className="search">
+                <input
+                    value={location}
+                    onChange={(e)=>handleLocationChange(e)}  
+                    onKeyDown={searchLocation}
+                    placeholder="Enter Location"
+                    type="text"/>
+            </div>
             <div className="container">
                 <div className="top">
                     <div className="location">
-                        <p>Dallas</p>
+                        <p>{data.name}</p>
                     </div>
                     <div className="tempature">
-                        <h1>60&#8457;</h1>
+                        {data.main ? <h1>{data.main.temp.toFixed()}&#8457;</h1> : null}
                     </div>
                     <div className="description">
-                        <p>Cloudy</p>
+                        {data.weather ? <p>{data.weather[0].main}</p> : null}
                     </div>
                 </div>
-                <div className="bottom">
+                {data.main &&
+                    <div className="bottom">
                     <div className="feels">
-                        <p className='bold'>65&#8457;</p>
+                        <p className='bold'>{data.main.feels_like.toFixed()}&#8457;</p>
                         <p>Feels Like</p>
                     </div>
                     <div className="humidity">
-                        <p className='bold'>20&#x25;</p>
+                        <p className='bold'>{data.main.humidity}&#x25;</p>
                         <p>Humidity</p>
                     </div>
                     <div className="wind">
-                        <p className='bold'>12 MPH</p>
+                        <p className='bold'>{data.wind.speed.toFixed()} MPH;</p>
                         <p>Wind Speed</p>
                     </div>
                 </div>
+            }
             </div>
         </div>
     )
